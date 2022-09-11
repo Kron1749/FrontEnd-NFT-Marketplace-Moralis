a42ef7a14779075e82c0793f491f328135ed77bc
Kron1749 <70746258+Kron1749@users.noreply.github.com>
2022-09-11 13:21:26 +0300

    Show recently listed NFTs on the marketplace

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,43 @@
 import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
+import NFTBox from "../components/NFTBox"
 import { useMoralisQuery } from "react-moralis"
 
 export default function Home() {
     const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
-        "ActiveItem", //tableName
+        // TableName
+        // Function for the query
+        "ActiveItem",
         (query) => query.limit(10).descending("tokenId")
     )
     console.log(listedNfts)
-    return <div className={styles.container}></div>
+    return (
+        <div className="container mx-auto">
+            <h1 className="py-4 px-4 font-bold text-2xl">Recently Listed</h1>
+            <div className="flex flex-wrap">
+                {fetchingListedNfts ? (
+                    <div>Loading...</div>
+                ) : (
+                    listedNfts.map((nft) => {
+                        console.log(nft.attributes)
+                        const { price, nftAddress, tokenId, marketplaceAddress, seller } =
+                            nft.attributes
+                        return (
+                            <div>
+                                <NFTBox
+                                    price={price}
+                                    nftAddress={nftAddress}
+                                    tokenId={tokenId}
+                                    marketplaceAddress={marketplaceAddress}
+                                    seller={seller}
+                                    key={`${nftAddress}${tokenId}`}
+                                />
+                            </div>
+                        )
+                    })
+                )}
+            </div>
+        </div>
+    )
 }
