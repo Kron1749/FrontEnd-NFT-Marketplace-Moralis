c932d25e457f2751e099981f0f85c9f89ece3524
Kron1749 <70746258+Kron1749@users.noreply.github.com>
2022-09-12 11:54:23 +0300

    Update Listing Price

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/globals.css"
 import Head from "next/head"
 import { MoralisProvider } from "react-moralis"
-
+import { NotificationProvider } from "web3uikit"
 import Header from "../components/Header"
 
 const APP_ID = process.env.NEXT_PUBLIC_APP_ID
@@ -16,8 +16,10 @@ function MyApp({ Component, pageProps }) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
-                <Header />
-                <Component {...pageProps} />
+                <NotificationProvider>
+                    <Header />
+                    <Component {...pageProps} />
+                </NotificationProvider>
             </MoralisProvider>
         </div>
     )
